be0179db1ec8aa81c5ff3ba5ac9400da5291c781
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-25 00:33:58 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,23 @@
+// Print 1 to 10 using for loop
+public class loops {
+    public static void main(String[] args){
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Print 10 to 1 using for loop
+
+package demo;
+
+public class loops {
+
+	public static void main(String[] args) {
+		for(int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
+
