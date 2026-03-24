8c793967df06d610bbe6b14840eb14c1cad2ae20
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-25 00:44:10 +0530

    /

--- loops.java
+++ loops.java
@@ -21,3 +21,18 @@ public class loops {
 
 }
 
+
+//Print even numbers from 1 to 20
+
+package demo;
+
+public class loops {
+	public static void main(String[] args) {
+		for(int i=1; i<=20; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+}
