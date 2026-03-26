43ddcb30fb5941cd1c3ed1828683fa69dc62cafb
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-26 23:35:59 +0530

    Star programming in java done

---
+++ starprogramming.java
@@ -0,0 +1,78 @@
+// Problem Set – Nested Loops (Easy)
+
+// Print a square of stars
+
+// * * *
+// * * *
+// * * *
+
+public class Nestedloops {
+    public static void main (String[] args){
+    for (int i = 1; i<4; i++){
+        for (int j = 1; j<3; j++) {
+            System.out.print(" *");
+        }
+    System.out.println(" *");
+    }
+    }
+}
+
+// *
+// * *
+// * * *
+// * * * *
+
+public class Main {
+    public static void main (String[] args) {
+    for (int i = 1; i<5; i++) {
+        for (int j = 1; j<=i; j++) {
+        System.out.print(" *");
+        }
+    System.out.println();
+        }
+    }
+}
+
+// * * * *
+// * * *
+// * *
+// *
+public class Main {
+    public static void main (String[] args) {
+    for (int i = 1; i<5; i++) {
+        int n = 5;
+        for (int j = 1; j<=n-i; j++) {
+        System.out.print(" *");
+        }
+    System.out.println();
+        }
+    }
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * *
+// * * *
+// * *
+// *
+public class Main {
+    public static void main (String[] args) {
+   for (int i = 1; i<5; i++) {
+        for (int j = 1; j<=i; j++) {
+        System.out.print(" *");
+        }
+    System.out.println();
+        }
+        
+        
+  for (int i = 1; i<5; i++) {
+        int n = 5;
+        for (int j = 1; j<=n-i; j++) {
+        System.out.print(" *");
+        }
+    System.out.println();
+        }
+    }
+}
