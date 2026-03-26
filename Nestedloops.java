a810508f37abb36881ae4a3fd99eb991ff75d0ed
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-26 22:26:15 +0530

    /

--- Nestedloops.java
+++ Nestedloops.java
@@ -6,7 +6,7 @@
 // * * *
 // * * *
 
-public class Main {
+public class Nestedloops {
     public static void main (String[] args){
     for (int i = 1; i<4; i++){
         for (int j = 1; j<3; j++) {
