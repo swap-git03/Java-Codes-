ac212e0aad15b9bd1083a778e3609c6282bb5175
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-28 10:53:01 +0530

    /

---
+++ methodCall.java
@@ -0,0 +1,11 @@
+// Call a Method
+public class Main {
+    static void myPrinter() {
+        System.out.println("Hii i am printer");
+    }
+    public static void main (String[] args){
+        myPrinter();
+        myPrinter(); 
+        myPrinter();
+    }
+}
