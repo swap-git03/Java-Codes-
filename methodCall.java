d2453327860b26b62bd0a58a655578180c4f1083
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-03-29 16:31:43 +0530

    good progress

--- methodCall.java
+++ methodCall.java
@@ -1,5 +1,5 @@
 // Call a Method
-public class Main {
+public class methodCall {
     static void myPrinter() {
         System.out.println("Hii i am printer");
     }
@@ -8,4 +8,61 @@ public class Main {
         myPrinter(); 
         myPrinter();
     }
+}
+
+// Params in method
+public class Main{
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + age);
+    }
+    public static void main (String[] args){
+        myMethod("Swap", 18);
+        myMethod("Swapnil", 184);
+        myMethod("Swapil", 182);
+
+    }
+}
+
+// Method to check can vote or not
+public class Main {
+    static void checkAge(int age){
+        if (age > 18) {
+            System.out.println("You can vote");
+        } else {
+            System.out.println("You can not vote");
+        }
+    }
+        public static void main (String[] args) {
+            checkAge(1);
+        }
+    }
+
+// Create a method that prints numbers from 1 to 10.
+class Main {
+    static void printNums(int num) {
+        for (int i = 1; i <= num; i++){
+            System.out.println(i);
+        }
+    }
+    public static void main (String[] args) {
+        printNums(21);
+
+    }
+}
+
+// Create a method that prints numbers from 1 to user input.
+import java.util.Scanner;
+class Main {
+    static void printNums(int num) {
+        for (int i = 1; i <= num; i++){
+            System.out.println(i);
+        }
+    }
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter ur num");
+        int n = sc.nextInt();
+
+        printNums(n);
+        }
 }
